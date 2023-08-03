bc6880870baf753f648de1f48f6e42ae4a745b53
math2do <mathura.dakshana15@gmail.com>
2023-08-03 19:52:28 +0530

    customise cursor in emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -147,7 +147,27 @@
 ;; show the key pressing events
 (use-package command-log-mode)
 
-;; compile c and c++ files with F4 -----------------------------------------------------------------------
+(setq cursor-type '(bar . 3))
+
+(defvar blink-cursor-colors (list  "#92c48f" "#6785c5" "#be369c" "#d9ca65")
+  "On each blink the cursor will cycle to the next color in this list.")
+
+(setq blink-cursor-count 0)
+(defun blink-cursor-timer-function ()
+  "Zarza wrote this cyberpunk variant of timer `blink-cursor-timer'. 
+Warning: overwrites original version in `frame.el'.
+
+This one changes the cursor color on each blink. Define colors in `blink-cursor-colors'."
+  (when (not (internal-show-cursor-p))
+    (when (>= blink-cursor-count (length blink-cursor-colors))
+      (setq blink-cursor-count 0))
+    (set-cursor-color (nth blink-cursor-count blink-cursor-colors))
+    (setq blink-cursor-count (+ 1 blink-cursor-count))
+    (internal-show-cursor nil (not (internal-show-cursor-p)))))
+
+(blink-cursor-timer-function)
+
+;; compile C++ files with F4 -----------------------------------------------------------------------
 
 (defun efs/code-compile ()
   (interactive)
@@ -862,10 +882,6 @@ folder, otherwise delete a word"
 
 (use-package docker
   :commands docker)
-
-(use-package docker-tramp
-  :defer t
-  :after docker)
 
 ;; treemacs ------------------------------------------------------------------------------------------------
 (use-package treemacs
