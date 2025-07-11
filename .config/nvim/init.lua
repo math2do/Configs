8c7982f782d29c6c02b786863b98bc7b50e7a549
math2do <mathura.dakshana15@gmail.com>
2025-07-11 09:43:59 +0000

    restructure the neo vim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -320,6 +320,7 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
