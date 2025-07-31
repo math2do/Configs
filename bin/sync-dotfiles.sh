3b7d12de93e70b014a1dd1e7a8732f49a530302c
math2do <mathura.dakshana15@gmail.com>
2025-07-31 16:26:57 +0000

    refactor the sync-dotfiles.sh

--- bin/sync-dotfiles.sh
+++ bin/sync-dotfiles.sh
@@ -15,7 +15,7 @@ echo -e "${BLUE}Stashing existing changes...${NC}"
 stash_result=$(git stash push -m "sync-dotfiles: Before syncing dotfiles")
 needs_pop=1
 if [ "$stash_result" = "No local changes to save" ]; then
-    needs_pop=0
+  needs_pop=0
 fi
 
 echo -e "${BLUE}Pulling updates from dotfiles repo...${NC}"
@@ -24,17 +24,17 @@ git pull origin wsl-main
 echo
 
 if [[ $needs_pop -eq 1 ]]; then
-    echo -e "${BLUE}Popping stashed changes...${NC}"
-    echo
-    git stash pop
+  echo -e "${BLUE}Popping stashed changes...${NC}"
+  echo
+  git stash pop
 fi
 
 unmerged_files=$(git diff --name-only --diff-filter=U)
 if [[ ! -z $unmerged_files ]]; then
-   echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
-   echo
-   printf %"s\n" $unmerged_files  # Ensure newlines are printed
+  echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
+  echo
+  printf %"s\n" $unmerged_files # Ensure newlines are printed
 else
-   # Run stow to ensure all new dotfiles are linked
-   stow .
+  # Run stow to ensure all new dotfiles are linked
+  stow .
 fi
