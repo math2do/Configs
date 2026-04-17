4c199ccfc3c2d8dc5ac13fc6b07cbad65dee271b
math2do <mathura.dakshana15@gmail.com>
2026-04-18 00:08:58 +0530

    add fzf aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,3 +36,32 @@ if type eza &>/dev/null; then
   alias lta4="eza -lTag --level=4 --icons=always"
   alias lta="eza -lTag --icons=always"
 fi
+
+# fzf (fuzzy finder)
+# ------------------------------------------------------------------------------
+if type fzf &>/dev/null; then
+  source <(fzf --zsh)
+
+  export FZF_CTRL_R_OPTS="
+  --color header:italic
+  --height=80%
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | xclip)+abort'
+  --header 'CTRL-Y: Copy command into clipboard, CTRL-/: Toggle line wrapping, CTRL-R: Toggle sorting by relevance'
+  "
+
+  export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --height=80%
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+  --header 'CTRL-/: Toggle preview window position'
+  "
+
+  export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'
+  --height=80%
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+  --header 'CTRL-/: Toggle preview window position'
+  "
+fi
