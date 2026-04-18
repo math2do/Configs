10abbcd421e3a8501564e93c76c70c64ac979fbd
math2do <mathura.dakshana15@gmail.com>
2026-04-18 16:02:11 +0530

    add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,8 @@
 # Aliases
 alias c="clear"
 alias cdp="cd ~/Projects/"
+alias cdg="cd ~/Projects/go/src/"
+alias cdj="cd ~/Projects/java/"
 alias cdc="cd ~/Configs"
 alias cdn="cd ~/Notes"
 alias clip="xclip -selection clipboard"
@@ -8,6 +10,10 @@ alias d='docker'
 alias docker-compose='docker compose'
 alias k='kubectl'
 alias h='helm'
+alias kg="kubectl get"
+alias kgp="kubectl get pods"
+alias kgs="kubectl get services"
+alias kd="kubectl describe"
 alias hf='helmfile'
 alias t='terraform'
 alias cl='clear'
