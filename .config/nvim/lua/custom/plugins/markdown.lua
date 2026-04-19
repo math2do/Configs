7fa61a9448f5721798490d5b1412bb59126aa877
math2do <mathura.dakshana15@gmail.com>
2026-04-19 21:38:46 +0530

    improve markdown theme

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -3,5 +3,14 @@ return {
 	version = '*',
 	enabled = true,
 	dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-	opts = {},
+	opts = {
+		heading = {
+			backgrounds = {}, -- disable heading backgrounds
+		},
+		code = {
+			disable_background = true, -- disable code block background
+			border = 'none', -- border style for code blocks (e.g., 'single', 'double', 'rounded', 'none')
+			highlight_border = false, -- whether to highlight the border of code blocks
+		},
+	},
 }
