1ceabac94147aad9821a0946ad4c2e780ea470ec
math2do <mathura.dakshana15@gmail.com>
2026-04-28 00:55:06 +0530

    use ts-comments

--- .config/nvim/lua/custom/plugins/comment.lua
+++ .config/nvim/lua/custom/plugins/comment.lua
@@ -1,7 +1,15 @@
 return -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 {
-	'numToStr/Comment.nvim',
-	opts = {
-		-- add any options here
+	-- {
+	-- 	'numToStr/Comment.nvim',
+	-- 	opts = {
+	-- 		-- add any options here
+	-- 	},
+	-- },
+	{
+		'folke/ts-comments.nvim',
+		opts = {},
+		event = 'VeryLazy',
+		ft = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'html' },
 	},
 }
